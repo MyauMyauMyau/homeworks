e9ab02d88839c10c4f3327d5d59d11252d9f0fa2
Sergey Avdonin <sunlf@yandex.ru>
2015-09-07 03:12:41 +0300

    hw1.beta

---
+++ 1/AngryBirds/AngryBirdsTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public class AngryBirdsTask
+	{
+        const double g = 9.8;
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+		public double FindSightAngle(double v, double distance)
+		{
+
+            return Math.Asin(distance*g/(v*v))/2;
+		}
+	}
+}
---
+++ 1/Billiards/BilliardTasks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Billiards
+{
+	public class BilliardTasks
+	{
+
+		public double BounceHorizontalWall(double directionRadians)
+		{
+			return (-1*directionRadians);
+        }
+
+		public double BounceWall(double directionRadians, double wallInclanationRadians)
+		{
+            //coordinates shift
+            return BounceHorizontalWall(directionRadians- wallInclanationRadians) + wallInclanationRadians;
+		}
+        public double BounceVerticalWall(double directionRadians)
+        {
+            return BounceWall(directionRadians, Math.PI / 2);
+        }
+    }
+}
